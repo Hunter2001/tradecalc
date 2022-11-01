9e5908a04acfea8283f75b2a63403d8d68380210
Hunter2001 <34722326+Hunter2001@users.noreply.github.com>
2022-11-02 00:10:20 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,14 +5,19 @@ const risk = document.querySelector('.risk');
 const stop = document.querySelector('.stop');
 const button = document.querySelector('.calculate');
 const output = document.querySelector('.output');
+const clipboard = document.querySelector('.clipboard');
 
 button.addEventListener('click', function (e) {
   e.preventDefault();
   const deposit2 = +deposit.value;
   const risk2 = +risk.value;
   const stop2 = +stop.value;
-  let output2 = Math.trunc((deposit2 * (risk2 / 100)) / (stop2 / 100));
+  let output2 = (deposit2 * (risk2 / 100)) / (stop2 / 100);
   output2 = output2 - output2 * 0.06;
-  output.textContent = output2;
+  const output3 = Math.trunc(output2);
+  output.textContent = output3;
   output.style.opacity = 1.0;
+  // clipboard
+  navigator.clipboard.writeText(output3);
+  clipboard.style.opacity = 1.0;
 });
